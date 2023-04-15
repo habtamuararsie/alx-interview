3bf5904948db73bb530e231e45f84dfdaba9ebcd
unknown <habtamuararsie@yahoo.com>
2023-04-15 18:38:05 +0300

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,47 +1,57 @@
 #!/usr/bin/python3
-"""Module
+"""  a script that reads stdin line by line and computes metrics
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size>
 """
 import sys
 
 
-def printStatus(status, fileSize):
+def display_message(dictionary_source, file_size):
     """
-    print the stats and size.
+    Method to display
+    Args:
+        dictionary_source: dict of status codes
+        file_size: total of the file
+    Returns:
+        Nothing to display
     """
-    try:
-        print('File size: {}'.format(fileSize))
-        [print("{}: {}".format(k, v))
-         for k, v in sorted(status.items()) if v != 0]
-    except Exception:
-        pass
+
+    print("File size: {}".format(file_size))
+    for key, val in sorted(dictionary_source.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
 
-status = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0,
-}
-fileSize = 0
-words = []
+T_file_size = 0
+message_code = 0
+count = 0
+dictonary_src = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
+
 try:
-    for i, line in enumerate(sys.stdin, 1):
-        words = line.split(' ')
+    for lines in sys.stdin:
+        pars_line = lines.split()  
+        pars_line = pars_line[::-1] 
 
-        try:
-            if (words[-2].isdigit() and words[-2] in status.keys()):
-                status[words[-2]] += 1
+        if len(pars_line) > 2:
+            count += 1
 
-            fileSize += int(words[-1].replace('\n', ''))
-        except Exception:
-            pass
-        if i % 10 == 0:
-            printStatus(status, fileSize)
-    printStatus(status, fileSize)
-except KeyboardInterrupt:
-    printStatus(status, fileSize)
-    raise
+            if count <= 10:
+                T_file_size += int(pars_line[0]) 
+                message_code = pars_line[1] 
+
+                if (message_code in dictonary_src.keys()):
+                    dictonary_src[message_code] += 1
+
+            if (count == 10):
+                display_message(dictonary_src, T_file_size)
+                count = 0
+
+finally:
+    display_message(dictonary_src, T_file_size)
