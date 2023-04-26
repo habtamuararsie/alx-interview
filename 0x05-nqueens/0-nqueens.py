7e83a299c249be98b5d0ada6f1d041795a338b63
unknown <habtamuararsie@yahoo.com>
2023-04-26 15:40:40 +0300

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,56 +1,31 @@
 #!/usr/bin/python3
-
-'''
-N queens
-'''
-
+"""[summary]"""
 from sys import argv
+argc = len(argv)
 
 
-def is_NQueen(cell: list) -> bool:
-    '''
-    True if N Queen, False if not
-    '''
-    row_number = len(cell) - 1
-    difference = 0
-    for index in range(0, row_number):
-        difference = cell[index] - cell[row_number]
-        if difference < 0:
-            difference *= -1
-        if difference == 0 or difference == row_number - index:
-            return False
-    return True
+def nqueens(num, row, res):
+    """Function"""
+    if row == num:
+        print(res)
+    for col in range(num):
+        if all(col != c and r + c != row + col and
+                r - c != row - col for r, c in res):
+            res.append([row, col])
+            nqueens(num, row + 1, res)
+            res.remove([row, col])
 
 
-def solve_NQeens(dimension: int, row: int, cell: list, outcome: list):
-    """
-    Return result of N Queens recusrivley
-    """
-    # Base case
-    if row == dimension:
-        print(outcome)
-    else:
-        for col in range(0, dimension):
-            cell.append(col)
-            outcome.append([row, col])
-            if (is_NQueen(cell)):
-                solve_NQeens(dimension, row + 1, cell, outcome)
-            cell.pop()
-            outcome.pop()
-
-
-if len(argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
-try:
-    N = int(argv[1])
-except BaseException:
-    print('N must be a number')
-    exit(1)
-if N < 4:
-    print('N must be at least 4')
-    exit(1)
-else:
-    outcome = []
-    cell = 0
-    solve_NQeens(int(N), cell, [], outcome)
+if __name__ == "__main__":
+    if argc != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    try:
+        num = int(argv[1])
+        if num < 4:
+            print("N must be at least 4")
+            exit(1)
+    except Exception:
+        print("N must be a number")
+        exit(1)
+    nqueens(num, 0, [])
