b5e75deb317764398fb20e38a55857854a1492c8
unknown <habtamuararsie@yahoo.com>
2023-05-04 11:48:37 +0300

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,45 @@
 #!/usr/bin/node
+const request = require('request');
+const filmId = process.argv.slice(1)[1];
 
-const request = require("request");
-
-const movieNum = process.argv[2];
-const URL = `https://swapi-api.alx-tools.com/api/films/${movieNum}`;
-
-// Makes API request, sets async to allow await promise
-request(URL, async function (err, res, body) {
-  if (err) return console.error(err);
-
-  // parse each character in the film as a list objects
-  const charURLList = JSON.parse(body).characters;
-
-  // Use URL list to character pages to make new requests
-  // await queues requests until they resolve in order
-  for (const charURL of charURLList) {
-    await new Promise(function (resolve, reject) {
-      request(charURL, function (err, res, body) {
-        if (err) return console.error(err);
-
-        // finds each character name and prints in URL order
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
-    });
+(function () {
+  if (!filmId || isNaN(parseInt(filmId))) {
+    return;
   }
-});
+
+  const getName = (character) =>
+    new Promise((resolve, reject) => {
+      request(
+        {
+          method: 'GET',
+          uri: character,
+          json: true
+        },
+        function (error, response, body) {
+          if (response.statusCode !== 200) {
+            console.error(error);
+          }
+          const { name } = body;
+          resolve(name);
+        }
+      );
+    });
+
+  request(
+    {
+      method: 'GET',
+      uri: `https://swapi-api.hbtn.io/api/films/${filmId}/`,
+      json: true
+    },
+    async function (error, response, body) {
+      if (response.statusCode !== 200) {
+        console.error(error);
+      }
+      const { characters } = body;
+      for (let i = 0; i < characters.length; i++) {
+        const name = await getName(characters[i]);
+        console.log(name);
+      }
+    }
+  );
+})();
