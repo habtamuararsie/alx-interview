b6bdf068324aad89202251bf7828aabd0b2ba520
unknown <habtamuararsie@yahoo.com>
2023-05-06 15:59:08 +0300

    starwar api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-
 const request = require('request');
+
 const Film_Id = process.argv[2];
 const Movie_url = {
   url: 'https://swapi-api.alx-tools.com/api/films/' + Film_Id,
@@ -10,6 +10,7 @@ const Movie_url = {
 
 request(Movie_url, function (error, response, body) {
   if (!error) {
+    
     const chars = JSON.parse(body).characters;
     print_Chars(chars, 0);
   }
@@ -18,6 +19,7 @@ request(Movie_url, function (error, response, body) {
 function print_Chars (chars, index) {
   request(chars[index], function (error, response, body) {
     if (!error) {
+
       console.log(JSON.parse(body).name);
       if (index + 1 < chars.length) {
         print_Chars(chars, index + 1);
