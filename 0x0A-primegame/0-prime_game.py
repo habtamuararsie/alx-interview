f816d2dd7af4408a6a8f26dc2291c6134cd60b58
unknown <habtamuararsie@yahoo.com>
2023-05-30 11:07:07 +0300

    primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """0. Prime Game - Maria and Ben are playing a game"""
 
 
@@ -7,13 +6,10 @@ def isWinner(x, nums):
     """x - rounds
     nums - numbers list
     """
-
     if x <= 0 or nums is None:
-
         return None
 
     if x != len(nums):
-
         return None
 
     ben = 0
@@ -21,21 +17,15 @@ def isWinner(x, nums):
 
     c = [1 for x in range(sorted(nums)[-1] + 1)]
     c[0], c[1] = 0, 0
-
     for j in range(2, len(c)):
-
         rm_multiples(c, j)
-
     for j in nums:
-
         if sum(c[0:j + 1]) % 2 == 0:
             ben += 1
         else:
             maria += 1
-
     if ben > maria:
         return "Ben"
-
     if maria > ben:
         return "Maria"
     return None
@@ -45,9 +35,7 @@ def rm_multiples(ls, x):
     """removes multiple
     of primes
     """
-
     for j in range(2, len(ls)):
-
         try:
             ls[j * x] = 0
         except (ValueError, IndexError):
